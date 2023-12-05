9a67e989d7e297ed90b846657d0be2de9dfac86b
adisnuhic <athis89@gmail.com>
2023-12-05 13:29:40 +0100

    added dynamic id to controller

--- internal/controllers/rest/user_controller.go
+++ internal/controllers/rest/user_controller.go
@@ -1,7 +1,11 @@
 package rest
 
 import (
+	"strconv"
+
+	"github.com/adisnuhic/go-clean/internal/ecode"
 	"github.com/adisnuhic/go-clean/internal/services"
+	"github.com/adisnuhic/go-clean/pkg/apperror"
 	"github.com/gin-gonic/gin"
 )
 
@@ -24,7 +28,13 @@ func NewUserController(svc services.IUserService) IUserController {
 
 // GetByID returns user by ID
 func (ctrl userController) GetByID(ctx *gin.Context) {
-	user, appErr := ctrl.Service.GetByID(12)
+	userID, err := strconv.ParseUint(ctx.Param("id"), 0, 64)
+	if err != nil {
+		ctrl.RenderBadRequest(ctx, apperror.New(ecode.ErrRequestParamValidationCode, err.Error(), ecode.ErrRequestParamValidationMsg))
+		return
+	}
+
+	user, appErr := ctrl.Service.GetByID(userID)
 
 	if appErr != nil {
 		ctrl.RenderError(ctx, appErr)
